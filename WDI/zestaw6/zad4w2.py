fc6a41590552b9a62e43918e44046c76ef007c61
youngbucu <lwilanskiwilan@gmail.com>
2022-12-14 21:02:38 +0100

    eelo

--- WDI/zestaw6/zad4w2.py
+++ WDI/zestaw6/zad4w2.py
@@ -1,23 +1,21 @@
 plansza=[]
-done=False
-moves=[[1,-2],[2,-1],[-2,-1],[-1,2],[-1,-2],[1,2],[-2,1],[2,1]]
+moves=[[-1,-2],[-1,2],[-2,-1],[2,-1],[1,-2],[1,2],[-2,1],[2,1]]
 
 def wyn(T):
     for i in T:
         print(i)
+    print("KONIEC")
         
 def skoczek(x=0,y=0,licz=2):
-    global plansza, done, moves
+    global plansza, moves
     for move in moves:
         if x+move[0]>-1 and x+move[0]<len(plansza) and y+move[1]>-1 and y+move[1]<len(plansza) and plansza[y+move[1]][x+move[0]]==0:
             plansza[y+move[1]][x+move[0]]=licz
             skoczek(x+move[0],y+move[1],licz+1)
-            if done:
-                return
     
     if licz==((len(plansza))**2)+1:
-        done=True
         wyn(plansza)
+        plansza[y][x]=0
         return
     
     plansza[y][x]=0
@@ -29,7 +27,6 @@ def main():
     plansza=[[0]*n for _ in range(n)]
     plansza[0][0]=1
     skoczek()
-    print("KONIEC")
     input()
 
 if __name__=='__main__':
---
+++ WDI/zestaw6/zad5.py
@@ -0,0 +1,55 @@
+import math as m
+
+T=[]
+found=False
+
+def bd(n):
+    wynik=0
+    p=len(str(n))-1
+    for i in str(n):
+        wynik+=int(i)*(2**p)
+        p-=1
+    return wynik
+        
+def isPrimeB(n):
+    n=bd(n)
+    if n==2:
+        return True
+    if n<2:
+        return False
+    for i in range(2,int(m.sqrt(n))+1):
+	    if n%i==0:
+		    return False
+    return True
+
+def rek(i=0):
+    global T, found
+    if i==len(T):
+        found=True
+        return
+    l=''
+    while True:
+        l+=str(T[i])
+        i+=1
+        if isPrimeB(int(l)):
+            print(l)
+            rek(i)
+        if i==len(T) or found:
+            return
+
+def main():
+    global T
+    T=[1,1,1,1,1]
+    rek()
+    print(found)
+
+if __name__=='__main__':
+    main()
+            
+        
+    
+        
+
+    
+
+
